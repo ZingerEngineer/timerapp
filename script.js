14e0198cc800e69f2c058ef5e38f06e7eb41756c
ZingerEngineer <ramyisez@gmail.com>
2021-10-01 20:27:31 +0200

    reset button made

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
 let t = document.querySelector(".timer");
 let btn1 = document.querySelector(".btnstart");
+let btn2 = document.querySelector("#btnreset");
 let s = 0;
 let h = 0;
 let d = 0;
 let m = 0;
 let timeArray = [d, h, m, s];
 let mytimer = null;
-t.innerHTML = timeArray.map((item) => String(item)).join(":");
+t.innerHTML = timeArray
+  .map((item) => String(item))
+  .map((item) => {
+    if (item.length == 1) {
+      return "0" + item;
+    } else {
+      return item;
+    }
+  })
+  .join(":");
+
+btn2.addEventListener("click", () => {
+  d = 0;
+  m = 0;
+  h = 0;
+  s = 0;
+  timeArray = [d, h, m, s];
+  t.innerHTML = timeArray
+    .map((item) => String(item))
+    .map((item) => {
+      if (item.length == 1) {
+        return "0" + item;
+      } else {
+        return item;
+      }
+    })
+    .join(":");
+    btn1.innerHTML="start"
+    stopmytimer();
+    btn2.classList.add("invisible");
+    btn2.classList.remove("visible");
+});
+if (btn1.innerHTML === "start" ){
+  btn2.classList.add("invisible");
+}
+
 function counter() {
   if (s == 60) {
     m++;
@@ -19,17 +55,35 @@ function counter() {
     h = 0;
   }
 }
+
 function mytimerstart() {
   s = s + 1;
   counter();
   timeArray = [d, h, m, s];
-  t.innerHTML = timeArray.map((item) => String(item)).join(":");
+  t.innerHTML = timeArray
+    .map((item) => String(item))
+    .map((item) => {
+      if (item.length == 1) {
+        return "0" + item;
+      } else {
+        return item;
+      }
+    })
+    .join(":");
 }
+
 function stopmytimer() {
   clearInterval(mytimer);
 }
 
 btn1.addEventListener("click", () => {
+  if (btn1.innerHTML === "start" || btn1.innerHTML === "resume") {
+    btn2.classList.add("invisible");
+    btn2.classList.remove("visible");
+  } else {
+    btn2.classList.add("visible");
+    btn2.classList.remove("invisible");
+  }
   if (btn1.innerHTML == "start") {
     mytimer = setInterval(mytimerstart, 1000);
     btn1.innerHTML = "stop";
